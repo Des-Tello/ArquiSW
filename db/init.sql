0550224826709f8e8949ce3df6936d4f323286f3
Benjamín Christian Tello Correa <benjamin.tello@docencia.udp.cl>
2023-05-28 20:54:40 +0000

    v3

--- db/init.sql
+++ db/init.sql
@@ -1,1 +1,10 @@
-CREATE TABLE Usuarios(Nombre VARCHAR(100) not null, Rut VARCHAR(100), Correo VARCHAR(100), Contrasena VARCHAR(100), Telefono VARCHAR(100), Rolt VARCHAR(100), Jardin VARCHAR(100), primary key(Rut));
+CREATE TABLE IF NOT EXISTS Usuarios(
+    Nombre VARCHAR(100) not null, 
+    Rut VARCHAR(100), 
+    Correo VARCHAR(100), 
+    Contrasena VARCHAR(100), 
+    Telefono VARCHAR(100), 
+    Rolt VARCHAR(100), 
+    Jardin VARCHAR(100), 
+    primary key(Rut)
+);
